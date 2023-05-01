88b35dde04b27fbe6de5c86f2ef46d066a6a8881
oliverLo78 <oliverberto@gmail.com>
2023-05-01 14:24:34 -0400

    sqlalchemy argument error for votes table

--- app/models/Post.py
+++ app/models/Post.py
@@ -11,12 +11,13 @@ class Post(Base):
     title = Column(String(100), nullable=False)
     post_url = Column(String(100), nullable=False)
     user_id = Column(Integer, ForeignKey('users.id'))
-    created_at = Column(DateTime, default=datetime.now)
-    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
     vote_count = column_property(
-        select([func.count(Vote.id)]).where(Vote.post_id == id)
+        select([func.count(Vote.id)]).where(Vote.post_id == id).label('vote_count')
     )
 
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    
     user = relationship('User')
     comments = relationship('Comment', cascade='all,delete')
     votes = relationship('Vote', cascade='all,delete')
