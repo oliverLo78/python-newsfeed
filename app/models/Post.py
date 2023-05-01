e2629c09ea4feb4644f7f4f5d83de8fd5c036731
oliverLo78 <oliverberto@gmail.com>
2023-05-01 13:45:31 -0400

    updated post model with dynamic property that performs a count!

--- app/models/Post.py
+++ app/models/Post.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 from app.db import Base
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from .Vote import Vote
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, select, func
 # app has one-to-many relationship between users and posts
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, column_property
 
 class Post(Base):
     __tablename__ = 'posts'
@@ -12,7 +13,10 @@ class Post(Base):
     user_id = Column(Integer, ForeignKey('users.id'))
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    vote_count = column_property(
+        select([func.count(Vote.id)]).where(Vote.post_id == id)
+    )
 
     user = relationship('User')
     comments = relationship('Comment', cascade='all,delete')
-
+    votes = relationship('Vote', cascade='all,delete')
