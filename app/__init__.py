474583ac3445f98e3079ca109b021f0bd3869f1e
oliverLo78 <oliverberto@gmail.com>
2023-05-01 09:02:40 -0400

    imported the blueprint object and added register routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from app.routes import home
 from flask import Flask
 
 def create_app(test_config=None):
@@ -12,5 +13,8 @@ def create_app(test_config=None):
   @app.route('/hello')
   def hello():
     return 'hello world'
+  
+  # register routes
+  app.register_blueprint(home)
   
   return app
